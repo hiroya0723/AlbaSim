ab831b41d8d61f8175a53662c67c88798cad60d1
kamino410 <kamino410@users.noreply.github.com>
2016-10-12 10:48:16 +0900

    メインシーンとコンポーネントスクリプトファイルの作成

---
+++ Assets/WingElement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WingElement : MonoBehaviour
+{
+    #region Unity Default Methods
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+    #endregion
+}
